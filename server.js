13d71d2d2dba619e5e13db48dbc3f85c2280fddc
Parker Shires <parkershires@gmail.com>
2018-03-05 16:52:27 -0500

    done

--- server.js
+++ server.js
@@ -5,4 +5,27 @@ const data = require('./db/notes');
 
 console.log('hello world!');
 
-// INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+
+const app = express();
+app.use(express.static('public'));
+
+
+app.get('/api/notes', (req, res) => {
+    res.json(data);
+});
+
+app.get('/api/notes:id', (req, res) => {
+    res.json(returnNoteById(req.params));
+});
+
+const returnNoteById = function (data) {
+    const {id} = params;
+    return data.find(item => item.id === Number(id));
+};
+
+app.listen(8080, function() {
+    console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+    console.error(err);
+});
