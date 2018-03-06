749a88f06f12b2a267f3d23569561b523fb4c756
Parker Shires <parkershires@gmail.com>
2018-03-06 17:56:35 -0500

    done for the day

--- server.js
+++ server.js
@@ -2,30 +2,92 @@
 
 // TEMP: Simple In-Memory Database
 const data = require('./db/notes');
+const simDB = require('./db/simDB');
+const notes = simDB.initialize(data);
+const express = require('express');
+const { PORT } = require('./config');
 
 console.log('hello world!');
 
-const express = require('express');
-
 const app = express();
 app.use(express.static('public'));
+app.use(express.json());
 
 
-app.get('/api/notes', (req, res) => {
-    res.json(data);
+const requestLogger = function (req, res, next) {
+    const now = new Date();
+    console.log(`Date: ${now.toLocaleDateString()} // Time: ${now.toLocaleTimeString()} // Method: ${req.method} // Url: ${req.url}`);
+    next();
+}
+
+app.use(requestLogger);
+
+app.get('/api/notes', (req, res, next) => {
+    const { searchTerm } = req.query;
+
+    notes.filter(searchTerm, (err, list) => {
+        if (err) {
+            return next(err);
+        }
+        res.json(list);
+    });
 });
 
-app.get('/api/notes:id', (req, res) => {
-    res.json(returnNoteById(req.params));
+app.put('/api/notes/:id', (req, res, next) => {
+    const { id } = req.params;
+    const updateObj = {};
+    const updateFields = ['title', 'content'];
+
+    updateFields.forEach(field => {
+        if (field in req.body) {
+            updateObj[field] = req.body[field];
+        }
+    });
+
+    notes.update(id, updateObj, (err, item) => {
+        if (err) {
+            return next(err);
+        }
+        if (item) {
+            res.json(item);
+        }
+        else {
+            next();
+        }
+    });
 });
 
-const returnNoteById = function (data) {
-    const {id} = params;
-    return data.find(item => item.id === Number(id));
-};
+app.get('/api/notes/:id', (req, res, next) => {
+    const { id } = req.params;
 
-app.listen(8080, function() {
-    console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-    console.error(err);
+    notes.find(id, (err, item) => {
+        if (err) {
+            return next(err);
+        }
+        if (item) {
+            res.json(item);
+        } else {
+            return next('not found');
+        }
+    })
 });
+
+    app.use(function (req, res, next) {
+        let err = new Error('Not Found');
+        err.status = 404;
+        res.status(404).json({ message: 'Not Found' });
+    });
+
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.json({
+            message: err.message,
+            error: err,
+        });
+    });
+
+    app.listen(PORT, function () {
+        console.info(`Server listening on ${this.address().port}`);
+    }).on('error', err => {
+        console.error(err);
+    });
